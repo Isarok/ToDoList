676710fdb9e0344697843af0639b85d88705eebd
Isarok <isaac.lucerito@gmail.com>
2024-01-04 14:20:53 +0100

    Log in

--- javascript/script.js
+++ javascript/script.js
@@ -122,3 +122,14 @@ window.onload = function () {
 }
 
 
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.querySelector('.menu-btn');
+    const navigation = document.querySelector('.navigation');
+
+    menuBtn.addEventListener('click', function () {
+        navigation.classList.toggle('show');
+        menuBtn.classList.toggle('open');
+    });
+});
