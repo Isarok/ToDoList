409065829d1bf272610e42483ef52692f5338ed1
Isarok <isaac.lucerito@gmail.com>
2024-01-05 11:28:38 +0100

    save

---
+++ javascript/login.js
@@ -0,0 +1,24 @@
+const deep = document.querySelector(".deep");
+const loginlink = document.querySelector(".login-link")
+const registerlink = document.querySelector(".register-link")
+const ntb = document.querySelector(".ntb")
+const closeicon = document.querySelector(".close-icon")
+
+
+registerlink.addEventListener("click", () => {
+    deep.classList.add('active');
+});
+
+loginlink.addEventListener("click", () =>{
+    deep.classList.remove('active');
+})
+
+ntb.addEventListener("click", () => {
+    deep.classList.add('active-ntb');
+})
+
+closeicon.addEventListener("click", () => {
+    deep.classList.remove("active-ntb");
+})
+
+
