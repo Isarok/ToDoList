d476b14bbb2945281273dbd74035f854dec0f8ae
Isarok <isaac.lucerito@gmail.com>
2024-10-07 01:17:30 +0200

    update URL login

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const signupButton = document.getElementById('signupButton');
 
     loginButton.addEventListener('click', function () {
-        window.location.href = 'http://127.0.0.1:5501/html/login.html#';
+        window.location.href = 'https://to-do-list-github-io-woad.vercel.app/html/login.html#';
     });
 
     signupButton.addEventListener('click', function () {
-        window.location.href = 'http://127.0.0.1:5501/html/login.html#';
+        window.location.href = 'https://to-do-list-github-io-woad.vercel.app/html/login.html#';
     });
 });
