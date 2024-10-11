858681ab1b0c765205d6db4a156ee902429ba94e
Isarok <isaac.lucerito@gmail.com>
2024-10-11 15:31:11 +0200

    refactor: update HTML structure and SEO practices
    Reorganized the head and body sections across all pages to improve semantic structure and enhance SEO compliance.

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const signupButton = document.getElementById('signupButton');
 
     loginButton.addEventListener('click', function () {
-        window.location.href = 'https://to-do-list-github-io-woad.vercel.app/html/login.html#';
+        window.location.href = 'http://127.0.0.1:5500/html/login.html';
     });
 
     signupButton.addEventListener('click', function () {
-        window.location.href = 'https://to-do-list-github-io-woad.vercel.app/html/login.html#';
+        window.location.href = 'http://127.0.0.1:5500/html/login.html';
     });
 });
+
+
+
+/* window.location.href = 'https://to-do-list-github-io-woad.vercel.app/html/login.html#'; */
